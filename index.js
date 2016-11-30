2567d6ca628e822ab4ca0af22d75b2296e4c5cfd
dannych <dnny.ch@gmail.com>
2016-11-30 17:09:38 +0700

    add response

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let controller = Botkit.slackbot({});
 let bot = controller.spawn({ 
     token: 'your-token'
 }).startRTM();
+
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,'Hello World!');
+});
